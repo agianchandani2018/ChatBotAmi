2ab8b62939713493b5b5b77d4b1d43525a818984
Ami <agianchandani2018@pingry.org>
2016-09-22 13:38:47 -0400

    with bye

---
+++ ChatBotAmi.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+/**
+ *This is a project
+ *
+ */
+ public class ChatBotAmi
+ {
+ 	/** This is the main method that java interpreter calls
+ 	  * @param args This is a command line argument
+ 	  */
+ 	public static void main(String[] args)
+ 	{
+ 		//create a new Scanner object
+ 		Scanner kb = new Scanner(System.in);  //get input from keyboard
+ 		System.out.print("Please enter your name: ");
+ 		String name = kb.nextLine();
+ 		System.out.println("Hi " + name + "! How are you doing today?");
+ 		String input = kb.nextLine();
+ 		System.out.println("Your answer is " + input);
+ 		System.out.println("How old are you? ");
+ 		int age = kb.nextInt ();
+ 		
+ 		if(age > 30)
+ 		{
+ 			System.out.println(name + ", you are old");
+ 				 
+ 		}
+ 		else
+ 		{
+ 			System.out.println("nice!");
+ 		}
+ 		System.out.print("How tall are you: " );
+ 		double height = kb.nextDouble();
+ 		if (height > 6.0)
+ 		{
+ 			System.out.println(name + ", you are tall!");
+ 		}
+ 		else if (height > 5.0)
+ 		{
+ 			System.out.println(name + ", good height");
+ 		}
+ 		else
+ 		{
+ 			 System.out.println(name + ", you are short");
+ 		} 
+ 		//will get rid of the new line character from previous input
+ 		kb.nextLine();
+		System.out.println("What is your favorite color? ");
+		String color = kb.nextLine();
+		System.out.println(color + " is my fav too!");
+
+
+		String animal = "";
+		while (! animal.equals("bye") && ! animal.equals("Bye"))
+		{
+			//kb.nextLine();
+			System.out.println("Name an animal");
+			animal = kb.nextLine();
+		}
+
+
+ 	}
+ }	
